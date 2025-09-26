0ed4e9db2def8506d63bb71f44e0c5141bdd9bbe
Yash Aghara <agharayash@gmail.com>
2025-09-26 20:49:08 +0530

    remove bat, hyprland config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,16 +1,26 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    # Set EDITOR, VISUAL, MANPAGER if nvim is available
+    if command -sq nvim
+        set -gx EDITOR nvim
+        set -gx VISUAL nvim
+        set -gx MANPAGER 'nvim +Man!'
+    end
+
+    # fzf key bindings
+    fzf --fish | source
+
+    # Starship prompt
+    starship init fish | source
 end
 
-fish_config theme choose "rose-pine"
-
-if command -sq nvim
-    set -gx EDITOR nvim
-    set -gx VISUAL nvim
-    set -gx MANPAGER 'nvim +Man!'
+# Activate mise if available
+if command -sq mise
+    mise activate fish | source
 end
 
-# Set up fzf key bindings
-fzf --fish | source
-
-starship init fish | source
+# Vi Mode
+# function fish_user_key_bindings
+#     fish_vi_key_bindings
+# end
